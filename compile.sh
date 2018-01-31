0dbfd55710d8f011818c71395e26f26b97582609
Patrick Kastner <patrick.kastner@gmail.com>
2018-01-31 07:01:12 +0100

    Create compile.sh

---
+++ compile.sh
@@ -0,0 +1,6 @@
+lualatex.exe -synctex=1 -interaction=nonstopmode thesis
+bibtex  thesis
+makeindex  thesis
+makeglossaries.exe  thesis
+lualatex.exe -synctex=1 -interaction=nonstopmode thesis
+call equations.bat
