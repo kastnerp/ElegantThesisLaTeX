89c70f6444bf312341d6f5a43e730b742f61e9a5
Patrick Kastner <patrick.kastner@gmail.com>
2018-12-10 10:11:52 -0500

    Update trim.bat

--- images/trim.bat
+++ images/trim.bat
@@ -1,3 +1,4 @@
 del *crop* /a /s
 for %%d in (*.*) do pdfcrop %%d
-for %%i in (*.*) do magick %%i -trim %%~ni-crop%%~xi
+for %%i in (*.png) do magick %%i -trim %%~ni-crop%%~xi
+for %%i in (*.jpg) do magick %%i -trim %%~ni-crop%%~xi
